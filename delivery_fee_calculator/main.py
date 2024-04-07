ed9dc927ddae928352774f1c81151c00ea9f066b
SamAddy <addisonsamuel70@yahoo.com>
2024-04-07 19:31:04 +0300

    Add route for getting homepage

--- delivery_fee_calculator/main.py
+++ delivery_fee_calculator/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from delivery_fee_calculator.fee_calculator import CalculateDeliveryFee
 from delivery_fee_calculator.schemas import DeliveryFeeRequest, DeliveryFeeResponse
@@ -21,6 +24,14 @@ app = FastAPI(
         "url": "https://samaddy.github.io/",
     }
 )
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(request, "index.html")
 
 
 @app.post("/fees", tags=["fees"], status_code=status.HTTP_201_CREATED)
